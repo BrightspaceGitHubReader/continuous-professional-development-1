ae9b87837da47eda3bd704b128c35bdae9ea1555
Joseph Wong <jwong@desire2learn.com>
2019-11-19 16:45:12 -0500

    factory for svc wrapper

--- src/services/cpd-records-svc.js
+++ src/services/cpd-records-svc.js
@@ -13,7 +13,7 @@ export class CpdRecordsService {
 	static getRecordSummary() {
 		const url = window.data.fraSettings.valenceHost;
 		const base_path = '/d2l/api/customization/cpd/1.0/record';
-		const getRecordSummaryRequest = new Request(url + base_path, {
+		const getRecordSummaryRequest = new Request('../data/cpd_records.json', {
 			method: 'GET',
 			headers: {
 				'Content-Type' : 'application/json'
