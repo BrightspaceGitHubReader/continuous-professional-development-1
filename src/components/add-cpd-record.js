b4b0db9c6d8dec8923d3f361fc544c59d4a6359a
Tom Michiels <tom.michiels@D2L.com>
2019-12-05 12:58:17 -0500

    fetching the data, and updating the record

--- src/components/add-cpd-record.js
+++ src/components/add-cpd-record.js
@@ -2,6 +2,7 @@ import '@brightspace-ui/core/components/inputs/input-text.js';
 import './attachments';
 import 'd2l-html-editor/d2l-html-editor';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { getHours, getMinutes } from '../helpers/time-helper.js';
 import { BaseMixin } from '../mixins/base-mixin.js';
 import { CpdRecordsServiceFactory } from '../services/cpd-records-service-factory';
 import { selectStyles } from '@brightspace-ui/core/components/inputs/input-select-styles.js';
@@ -109,6 +110,7 @@ class AddCpdRecord extends BaseMixin(LitElement) {
 								href: `${window.data.fraSettings.valenceHost}${file.Href}`
 							};
 						});
+						this.attachments = body.Attachments.Files;
 					}
 					this.record = body;
 				});
@@ -139,7 +141,7 @@ class AddCpdRecord extends BaseMixin(LitElement) {
 			SubjectId: this.shadowRoot.querySelector('#subjectSelect').value,
 			IsStructured: !!+this.shadowRoot.querySelector('#typeSelect').value,
 			MethodId: this.shadowRoot.querySelector('#methodSelect').value,
-			CreditMinutes: this.shadowRoot.querySelector('#creditHours').value * 60 + this.shadowRoot.querySelector('#creditMinutes').value,
+			CreditMinutes: parseInt((this.shadowRoot.querySelector('#creditHours').value || 0) * 60) + parseInt((this.shadowRoot.querySelector('#creditMinutes').value || 0)),
 			Answers: this.questions.map(question => {
 				return {
 					QuestionId: question.Id,
@@ -147,7 +149,22 @@ class AddCpdRecord extends BaseMixin(LitElement) {
 				};
 			})
 		};
+		if (this.recordId) {
+			return this.SaveUpdatedRecord(record);
+		}
 		this.cpdRecordService.createRecord(record, this.attachments);
+		this.fireNavigateMyCpdEvent();
+	}
+
+	SaveUpdatedRecord(record) {
+		const newAttachments = this.attachments.filter(f => f instanceof File);
+		let removedAttachments = [];
+		if (this.record.Attachments) {
+			removedAttachments = this.record.Attachments.Files
+				.filter(oldFile => !this.attachments.includes(oldFile))
+				.map(f => f.id);
+		}
+		this.cpdRecordService.updateRecord(this.recordId, record, newAttachments, removedAttachments);
 		this.fireNavigateMyCpdEvent();
 	}
 
@@ -200,8 +217,8 @@ class AddCpdRecord extends BaseMixin(LitElement) {
 						<div>
 							<div>
 								<label for="creditHours" class=d2l-label-text>${this.localize('credits')}</label>
-								<d2l-input-text id="creditHours" placeholder=${this.localize('enterCreditHours')} type="number" min="0" value="${this.record && this.record.CreditMinutes || ''}"></d2l-input-text>
-								<d2l-input-text class="numberInput" id="creditMinutes" placeholder=${this.localize('enterCreditMinutes')} type="number" min="0" max="59"  value="${this.record && this.record.CreditMinutes || ''}"></d2l-input-text>
+								<d2l-input-text id="creditHours" placeholder=${this.localize('enterCreditHours')} type="number" min="0" value="${this.record && getHours(this.record.CreditMinutes) || ''}"></d2l-input-text>
+								<d2l-input-text class="numberInput" id="creditMinutes" placeholder=${this.localize('enterCreditMinutes')} type="number" min="0" max="59"  value="${this.record && getMinutes(this.record.CreditMinutes) || ''}"></d2l-input-text>
 							</div>
 							${this.record && this.record.Grade ? html`
 								<div>
@@ -213,7 +230,7 @@ class AddCpdRecord extends BaseMixin(LitElement) {
 					</li>
 					<li>
 						<label>${this.localize('addEvidence')}</label>
-						<d2l-attachments attachmentslist="${JSON.stringify(this.record && this.record.Attachments && this.record.Attachments.Files || [])}" @d2l-attachments-list-updated="${this.attachmentsUpdated}"></d2l-attachments>
+						<d2l-attachments .attachmentsList="${this.attachments}" @d2l-attachments-list-updated="${this.attachmentsUpdated}"></d2l-attachments>
 					<li>
 					${this.questions.map((q) => this.renderQuestion(q))}
 				</ul>
